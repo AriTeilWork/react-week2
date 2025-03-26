178b5a940c5447a84218d37bf8b231042e2cfd06
AriTeil <118675580+AriTeilWork@users.noreply.github.com>
2025-03-26 18:13:00 +0200

    Fixes redy

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,17 @@ import Greeting from './components/Greeting';
 import NameInput from './components/NameInput';
 import Weekdays from './components/Weekdays';
 
-const App = () => {
+function App() {
   const [name, setName] = useState('');
 
   return (
     <div>
+      <h1>Welcome to React Week 2</h1>
       <NameInput name={name} setName={setName} />
       <Greeting name={name} />
       <Weekdays />
     </div>
   );
-};
+}
 
 export default App;
