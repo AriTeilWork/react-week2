87050b3b53dd188268dfe33da5accfe2b341109e
AriTeil <118675580+AriTeilWork@users.noreply.github.com>
2025-03-26 18:04:36 +0200

    Fix page dependencies

--- src/components/Weekdays.jsx
+++ src/components/Weekdays.jsx
@@ -4,11 +4,14 @@ const Weekdays = () => {
   const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
   const today = new Date().getDay();
   const orderedWeekdays = [...weekdays.slice(today), ...weekdays.slice(0, today)];
-    return (
+
+  return (
     <ul>
       {orderedWeekdays.map((weekday, index) => (
         <li key={index}>{weekday}</li>
       ))}
     </ul>
-    );
+  );
 };
+
+export default Weekdays;
