bc8cdc29078ce6ec6ab278495b6ed5995309e3d6
AriTeil <118675580+AriTeilWork@users.noreply.github.com>
2025-03-26 17:36:31 +0200

    return  <ul>...</ul> element wits weekdays starting from today

--- src/components/Weekdays.jsx
+++ src/components/Weekdays.jsx
@@ -4,5 +4,11 @@ const Weekdays = () => {
   const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
   const today = new Date().getDay();
   const orderedWeekdays = [...weekdays.slice(today), ...weekdays.slice(0, today)];
-    return ();
+    return (
+    <ul>
+      {orderedWeekdays.map((weekday, index) => (
+        <li key={index}>{weekday}</li>
+      ))}
+    </ul>
+    );
 };
